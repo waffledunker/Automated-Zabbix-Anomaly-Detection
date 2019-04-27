a2b4d12b62c6d1fe990f560db7adbc35e4474471
alpcan iscan <ce.alpcan@windowslive.com>
2019-04-28 00:23:58 +0300

    Add files via upload
    ARIMA pdq parameter finding is updated. And some minor changes.

--- automationscript.py
+++ automationscript.py
@@ -19,11 +19,12 @@ from math import sqrt
 import os
 import itertools
 import warnings
+from RegscorePy import *
 
 
 # PART 1
 #zabbix server url
-zapi = ZabbixAPI("http://yourmachineip/zabbix") 
+zapi = ZabbixAPI("http://192.168.56.101/zabbix") 
 #zabbix creds
 zapi.login("Admin", "zabbix")
 
@@ -35,6 +36,12 @@ global bool_value
 bool_value = True
 global iterator
 iterator = 0
+
+#variables for arima parameters and aic function value
+global param_temp
+global aic_temp
+global mse_temp
+
 
 #while loop to check if filename allready exists
 while bool_value:
@@ -52,9 +59,7 @@ while bool_value:
 filename = "zabbixdata_%d_.json" % iterator
 #ForecastCPU values(float) sorted by clock,json data. (select itemid,key_,name from items where key_="anything";)
 #select itemid,name,key_ form items; to display full list of items.
-
-#specify itemid which you want to predict
-for h in zapi.history.get(history='0',itemids='23725', sortfield='clock', sortorder='DESC',limit=500):
+for h in zapi.history.get(history='0',itemids='23303', sortfield='clock', sortorder='DESC',limit=500):
 	#convert dict data to json 
 	h = json.dumps(dict(h))
 	#write json data to file
@@ -126,33 +131,76 @@ predictions = []
 
 #----------------------- END ------------------------
 
-
 #PART 3
+#--------------------------------OPTIONAL-----------------------------------------
+#now we are trying to find best combination of parameters for arima
+
+warnings.filterwarnings('ignore')
+
+#we can appends this range to find best combination if we havent yet.
+p=d=q= range(0,6)
+
+pdq = list(itertools.product(p,d,q))
+print(pdq)
+
+#now we make for loop to iterate over
+
+for param in pdq:
+	
+	try:
+		model_arima = ARIMA(train, order= param)
+		model_arima_fit = model_arima.fit()
+		print(mean_squared_error, param, model_arima_fit.aic)
+		
+		aic_temp = model_arima_fit.aic
+		param_temp = param
+		mse_temp = mean_squared_error
+
+		if aic_temp < model_arima_fit.aic & model_arima_fit.aic > 0:
+			
+			mse_temp = mean_squared_error
+			param_temp = param
+			aic_temp = model_arima_fit.aic
+			print("Better parameter combination found!")
+		elif aic_temp > model_arima_fit.aic & model_arima_fit.aic < 0:
+			mse_temp = mean_squared_error
+			param_temp = param
+			aic_temp = model_arima_fit.aic
+	except:
+		continue
+
+
+#after determination of best parameter combination,we plot the graph
+
+#----------------------END----------------------
+
+#PART 4
 #ARIMA Model First Part(Auto Regression(AR))
 
 #p,d,q parameters correspond to AR,I,MA parameters.
 #periods, order of diff, lag
 
-#3.1
+#4.1
 
-#p=5, d=1, q=1
-model_arima = ARIMA(train, order = (4,2,4))
+#we pass param_temp value which was fetched from aic trials
+model_arima = ARIMA(train, order = param_temp)
 
 #substract each value from the one that follows it. thus allows graph to be smoother.
 model_arima_fit = model_arima.fit()
 
-#3.2
+#4.2
 
 #we apply forecast(in arima,we use forecast func instead of predict) and store it in predictions
 predictions = model_arima_fit.forecast(steps = 49)[0]
 #predictions = np.append(predictions)
 
+
 #test as red,predictions as yellow color
-plt.plot(test, color = 'red')
-plt.plot(predictions, color = 'yellow')
+plt.plot(test,color = 'red')
+plt.plot(predictions, color ='blue')
+plt.show()
 
-
-#3.3
+#4.3
 
 #mean square error
 mse = mean_squared_error(test,predictions)
@@ -167,26 +215,6 @@ plt.show()
 #-----------------END-------------------
 
 
-#PART 4
-#--------------------------------OPTIONAL-----------------------------------------
-#now we are trying to find best combination of parameters for arima
 
-warnings.filterwarnings('ignore')
-
-#we can appends this range to find best combination if we havent yet.
-p=d=q= range(0,5)
-
-pdq = list(itertools.product(p,d,q))
-print(pdq)
-
-#now we make for loop to iterate over
-
-for param in pdq:
-	try:
-		model_arima = ARIMA(train, order= param)
-		model_arima_fit = model_arima.fit()
-		print(mean_squared_error,param,model_arima_fit.aic)
-	except:
-		continue
 
 
