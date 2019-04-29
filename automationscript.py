c0f3a61122ba137447cd48e8061975a89f4e49ec
alpcan iscan <ce.alpcan@windowslive.com>
2019-04-29 21:51:42 +0300

    Add files via upload
    #minor changes and implementing argparser.

--- automationscript.py
+++ automationscript.py
@@ -1,8 +1,10 @@
 #               ---------TO DO --------------
 
-#make system argv input enter(param_temp,range_temp,ip_addr,itemid,saveoutput_graphs) 
+#make system argv input enter(not finished) 
 #add plot labels
-#data persentage between train and prediction
+#check pdq combination selector,global variables
+#save file name with date
+#check imports,modules are exist on machine(importlib)
 # 				--------- TO DO ------------
 
 
@@ -16,10 +18,53 @@ from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 import os
+import argparse
 import itertools
 import warnings
+import importlib
+
+# PART 0 
+
+#Argument parsing
+
+#url parse
+parser = argparse.ArgumentParser(description='Automated Zabbix Anomaly Detector')
+parser.add_argument('-ip', type = str, default = 'http://localhost/zabbix',
+	help = 'http(s)://<your_zabbix_machine_url/zabbix>  Note: Check your port value.This program assumes it is "3000".')
+#itemid parse
+parser.add_argument('-id' type = int, default = '23303', 
+	help = '<itemid> , ex = 20303(CPU Softrq Time)')
+
+#Zabbix username
+parser.add_argument('-u', type = str, default = 'Admin', 
+	help = '<username_of_zabbix_login_screen>, default:"<Admin>" . Note: Make sure,selected user has auth to read data.')
+
+#Zabbix password
+parser.add_argument('-p', type = str, default = 'zabbix', 
+	help = '<password_of_zabbix_login_screen>, default:"<zabbix>"')
+
+#P,D,Q range
+parser.add_argument('-r', type = int, default = 5,
+	help = '<value> , Advised to use stock value(5) if you have no clue. ARIMA p,d,q variables range. This range will determine how many combinations will be.ex =[0,0,0] to [5,5,5]. This will effect performance of your computer.Higher value, will take longer time.'.)
+
+#Plot graphs save name
+parser.add_argument('-o' type = str, default = 'plots.pdf', 
+	help = '<filename.extention> , Save generated plots to your current directory with a selected name. ex: "plots.pdf".')
 
 
+#now we pars the args
+
+if len(args) < 3:
+	print('Requires at least 2 arguments(-ip, -id) to be filled.')
+
+args = parser.parse_args()
+
+print('Entered Arguments are : {} , {}, {}, {}, {}, {}'.format(args[1],args[2],args[3],args[4],args[5],args[6]))
+
+
+'''
+
+# ---------------------  END  ---------------
 # PART 1
 #zabbix server url
 zapi = ZabbixAPI("http://192.168.56.101/zabbix") 
@@ -42,6 +87,9 @@ global mse_temp
 global range_temp
 range_temp = 8
 
+#plot images output address
+plt_out_addr = ?
+
 
 #while loop to check if filename allready exists
 while bool_value:
@@ -59,7 +107,7 @@ while bool_value:
 filename = "zabbixdata_%d_.json" % iterator
 #ForecastCPU values(float) sorted by clock,json data. (select itemid,key_,name from items where key_="anything";)
 #select itemid,name,key_ form items; to display full list of items.
-for h in zapi.history.get(history='0',itemids='23303', sortfield='clock', sortorder='DESC',limit=500):
+for h in zapi.history.get(history='0',itemids='23303', sortfield='clock', sortorder='DESC',limit=501):
 	#convert dict data to json 
 	h = json.dumps(dict(h))
 	#write json data to file
@@ -113,16 +161,32 @@ plot_acf(tseries_parsed_diff, color= 'brown')
 #2.3
 #train and prediction values will be set
 
+#check how many values are there,which will be 499(because we exctracted 1 by selecting clock as an index).
 X = tseries_parsed.values
-#check how many values are there,which will be 499.
+
+#train and prediction data variables
+global X_TNEW
+global X_PNEW
+
+#check if X size is even number
+if X.size % 2 == 0:
+	#X_TNEW = Train data %90
+	X_TNEW = X[0:int(len(X)*0.9)]
+	#X_PNEW = Predic data %10
+	X_PNEW = X[int(len(X)*0.9):]
+#check if X size is odd number	
+if X.size % 2 == 1:
+	X_TNEW = X[0:int(len(X)*0.9)]
+	X_PNEW = X[int(len(X)*0.9):]
+
 print("loaded data count: %d \n"% (X.size))
 
 #train data is set to train variable
-train = X[0:450] 
+train = X_TNEW 
 print("train data count : %d \n" % (train.size))
 
 #test data is set to test variable
-test = X[450:499]
+test = X_PNEW
 print("test data count : %d \n" % (test.size))
 
 #predictions variable
@@ -195,8 +259,7 @@ model_arima_fit = model_arima.fit()
 #4.2
 
 #we apply forecast(in arima,we use forecast func instead of predict) and store it in predictions
-predictions = model_arima_fit.forecast(steps = 49)[0]
-#predictions = np.append(predictions)
+predictions = model_arima_fit.forecast(steps = X_PNEW.size)[0]
 
 
 #test as red,predictions as yellow color
@@ -214,6 +277,7 @@ print("Test result: {} ".format(mse))
 print("RMSE : %3.f \n" % rmse)
 
 plt.show()
+plt.savefig(plt_out_addr)
 
 #-----------------END-------------------
 
@@ -221,3 +285,4 @@ plt.show()
 
 
 
+'''
